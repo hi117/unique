a6e1e51eda4b2fbeb47f2555a471c920ef32cf31
Yanus Poluektovich <ypoluektovich@gmail.com>
2012-06-22 00:53:28 +0400

    Proper serialization and synchronization for Timeline

--- src/java/net/hi117/unique/Timeline.java
+++ src/java/net/hi117/unique/Timeline.java
@@ -1,6 +1,14 @@
 package net.hi117.unique;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * An object that encapsulates the timeline and handles the progression of
@@ -8,54 +16,124 @@ import java.util.PriorityQueue;
  *
  * @author Yanus Poluektovich (ypoluektovich@gmail.com)
  */
-public class Timeline {
-	private final PriorityQueue<Event> myQueue =
+public final class Timeline implements Serializable {
+
+	private final Queue<Event> myQueue =
 			new PriorityQueue<>(16, EventComparator.INSTANCE);
 
 	private long myCurrentTime;
 
 	private int myCurrentPriority = Integer.MIN_VALUE;
 
-	public void addEvent(final Event event) throws CausalityViolationException {
-		final long newTime = event.getTime();
-		if (myCurrentTime > newTime) {
-			throw new CausalityViolationException(event, myQueue.peek());
-		}
-		if (myCurrentTime == newTime &&
-				event.getPriority() <= myCurrentPriority) {
-			throw new CausalityViolationException(event, myQueue.peek());
-		}
-		myQueue.add(event);
+
+	public Timeline() {
+		// do nothing
 	}
 
-	public boolean tick()
+
+	/* Serialization */
+
+	private Timeline(final Event[] events) {
+		myQueue.addAll(Arrays.asList(events));
+	}
+
+	private static final class SerialProxy implements Serializable {
+		private static final long serialVersionUID = 1L;
+		private transient Event[] myEvents;
+
+		protected SerialProxy(final Collection<Event> events) {
+			myEvents = events.toArray(new Event[events.size()]);
+		}
+
+		private void writeObject(final ObjectOutputStream s)
+				throws IOException {
+			s.defaultWriteObject();
+			s.writeInt(myEvents.length);
+			for (final Event event : myEvents) {
+				s.writeObject(event);
+			}
+		}
+
+		private void readObject(final ObjectInputStream s)
+				throws ClassNotFoundException, IOException {
+			s.defaultReadObject();
+			final int eventCount = s.readInt();
+			final Event[] events = new Event[eventCount];
+			for (int i = 0; i < eventCount; ++i) {
+				events[i] = (Event) s.readObject();
+			}
+			myEvents = events;
+		}
+
+		private Object readResolve() {
+			return new Timeline(myEvents);
+		}
+	}
+
+	private void readObject(final ObjectInputStream s)
+			throws InvalidObjectException {
+		throw new InvalidObjectException("Proxy required!");
+	}
+
+	private Object writeReplace() {
+		synchronized (myQueue) {
+			return new SerialProxy(myQueue);
+		}
+	}
+
+
+	public final boolean tick()
 			throws CausalityViolationException, EventException,
 			       InterruptedException {
 		if (!setCurrentTime()) {
 			return false;
 		}
-		Event event;
-		while ((event = myQueue.peek()) != null) {
-			if (event.getTime() != myCurrentTime) {
-				break;
+		while (true) {
+			final Event event;
+			synchronized (myQueue) {
+				event = myQueue.peek();
+				if (event == null || event.getTime() != myCurrentTime) {
+					break;
+				}
+				myCurrentPriority = event.getPriority();
 			}
-			myCurrentPriority = event.getPriority();
 			event.trigger();
-			myQueue.poll();
+			synchronized (myQueue) {
+				myQueue.remove(event);
+			}
 		}
 		return true;
+	}
+
+	public final void addEvent(final Event event)
+			throws CausalityViolationException {
+		synchronized (myQueue) {
+			final long newTime = event.getTime();
+			if (myCurrentTime > newTime) {
+				throw new CausalityViolationException(event, myQueue.peek());
+			}
+			if (myCurrentTime == newTime &&
+					event.getPriority() <= myCurrentPriority) {
+				throw new CausalityViolationException(event, myQueue.peek());
+			}
+			myQueue.add(event);
+		}
+	}
+
+	public final long getCurrentTime() {
+		synchronized (myQueue) {
+			return myCurrentTime;
+		}
 	}
 
 	private boolean setCurrentTime() {
-		final Event peek = myQueue.peek();
-		if (peek == null) {
-			return false;
+		synchronized (myQueue) {
+			final Event peek = myQueue.peek();
+			if (peek == null) {
+				return false;
+			}
+			myCurrentTime = peek.getTime();
+			return true;
 		}
-		myCurrentTime = peek.getTime();
-		return true;
-	}
-
-	public long getCurrentTime() {
-		return myCurrentTime;
 	}
 }
